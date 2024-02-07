c8de5ff4648f0447805db8d5b14bb30a9fb0ded0
Anna work laptop <a.m.van.harmelen@vu.nl>
2024-02-07 13:51:13 +0100

    check response functionality

--- debug.py
+++ debug.py
@@ -11,17 +11,24 @@ import time
 from set_up import get_monitor_and_dir, get_settings
 from trial import generate_stimuli_characteristics, single_trial
 from block import create_blocks
+from response import get_response
 
-blocks = create_blocks(6)
-print(blocks)
+monitor, directory = get_monitor_and_dir(True)
+settings = get_settings(monitor, directory)
+
+try:
+    get_response("colour", 45, "#ff0000", "congruent", "left", settings, True, None)
+except Exception as e:
+    print(e)
 
 # stop here
 import sys
 
 sys.exit()
 
-monitor, directory = get_monitor_and_dir(True)
-settings = get_settings(monitor, directory)
+blocks = create_blocks(6)
+print(blocks)
+
 window = settings["window"]
 deg2pix = settings["deg2pix"]
 
