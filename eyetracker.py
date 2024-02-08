7ea28c0ac071fd52d6b7f4a4d7802c61a81cfc8b
Anna work laptop <a.m.van.harmelen@vu.nl>
2024-02-08 15:34:11 +0100

    update get_trigger() to new triggers

--- eyetracker.py
+++ eyetracker.py
@@ -48,17 +48,24 @@ class Eyelinker:
         self.tracker.close_edf()
 
 
-def get_trigger(frame, condition, target_position):
-    condition_marker = {"congruent": 1, "incongruent": 3, "neutral": 5}[condition]
+def get_trigger(frame, probe_form, capture_form, congruency, target_position):
+    condition_marker = {"location_probe": 1, "colour_probe": 9}[probe_form]
+
+    if capture_form == "colour_cue":
+        condition_marker += 4
+
+    if congruency == "incongruent":
+        condition_marker += 2
 
     if target_position == "right":
         condition_marker += 1
 
     return {
-        "stimuli_onset": "",
-        "capture_cue_onset": "1",
-        "probe_cue_onset": "2",
-        "response_onset": "3",
-        "response_offset": "4",
-        "feedback_onset": "5",
+        "just_code_please": "",
+        "stimuli_onset": "1",
+        "capture_cue_onset": "2",
+        "probe_cue_onset": "3",
+        "response_onset": "4",
+        "response_offset": "5",
+        "feedback_onset": "6",
     }[frame] + str(condition_marker)
